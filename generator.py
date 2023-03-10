942c6d57366f7e4e80cf82ebbcd83fb9efe287f7
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-03-10 02:00:19 +0000

    Code: erased flood fill code and implemented randomized DFS to generate empty walls.

--- generator.py
+++ generator.py
@@ -1,25 +1,30 @@
 import random
+import math
 import os
 from colorama import Style
 from colorama import Fore
 
-# COLOR MACROS
+#! Colors
 RESET = Style.RESET_ALL
 LRED = Fore.LIGHTRED_EX
 LYELLOW = Fore.LIGHTYELLOW_EX
 LBLUE = Fore.LIGHTBLUE_EX
 LGREEN = Fore.LIGHTGREEN_EX
 
-# OTHER MACROS
+#! Other macros
 GEN_PATH = "maps/generated"
 WALL_PROBABILITY = 0.80
 COIN_PROBABILITY = 0.80
-HORIZONTAL = 0
-VERTICAL = 1
-DIRECTIONS = [HORIZONTAL, VERTICAL]
+
+#! Entities
+WALL = "1"
+PLAYER = "P"
+COIN = "C"
+EXIT = "E"
+EMPTY = "0"
 
 def generate_coins(contents, x, y) -> None:
-	num_coins = random.randint(0, max(x, y) * COIN_PROBABILITY)
+	num_coins = random.randint(0, math.floor((max(x, y) * COIN_PROBABILITY)))
 	print(f"Num coins: {num_coins}")
 
 	for i in range(0, num_coins):
@@ -42,52 +47,46 @@ def generate_player_and_exit(contents, x, y) -> None:
 	contents[py][px] = 'P'
 	contents[ey][ex] = 'E'
 
-def flood_fill(contents, point, dims, prob):
-	if contents[point[1]][point[0]] != '0' or \
-		point[1] not in range(0, dims[1] - 1) or \
-		point[0] not in range(0, dims[0] - 1):
-		return
-	
-	n = random.randint(1, 100)
-	if n in range(0, 100 - prob):
-		contents[point[1]][point[0]] = '1'
-		return
-	flood_fill(contents, [point[0] - 1, point[1]], dims, int(prob * WALL_PROBABILITY))	#L
-	flood_fill(contents, [point[0] + 1, point[1]], dims, int(prob * WALL_PROBABILITY)) 	#R
-	flood_fill(contents, [point[0], point[1] - 1], dims, int(prob * WALL_PROBABILITY))	#U
-	flood_fill(contents, [point[0], point[1] + 1], dims, int(prob * WALL_PROBABILITY))	#D
+def get_random_unvisited_node(visited, x, y):
+
+	unvisited_nodes = []
+
+	if not visited[y - 1][x]:
+		unvisited_nodes.append([y - 1, x])
+	if not visited[y + 1][x]:
+		unvisited_nodes.append([y + 1, x])
+	if not visited[y][x - 1]:
+		unvisited_nodes.append([y, x - 1])
+	if not visited[y][x + 1]:
+		unvisited_nodes.append([y, x + 1])
+	if len(unvisited_nodes) < 2:
+		return None
+	return unvisited_nodes[random.randint(0, len(unvisited_nodes) - 1)]
+
+def randomized_dfs(contents, x, y, visited):
+	visited[y][x] = True
+	contents[y][x] = EMPTY
+	pos = get_random_unvisited_node(visited, x, y)
+
+	while pos != None:
+		randomized_dfs(contents, pos[1], pos[0], visited)
+		pos = get_random_unvisited_node(visited, x, y)
+		
 
 def generate_walls(contents, x, y):
-	for line in contents[2::2]:
-		px = random.randint(0, x - 1)
-		width = random.randint(0, x - 1)
-		
-		#print(f'{line} -> x = {px} width = {width}')
-		i = 0
-		for i in range (0, x - 1):
-			if line[i] == '0' and i in range(px, px + width):
-				line[i] = '1'
-			i += 1
-	
-	for i in range(0, x - 1, 3):
-		py = random.randint(0, y - 1)
-		width = random.randint(0, y - 1)
-		
-		#print(f'col -> y = {py} width = {width}')
-		for k in range (0, y - 1):
-			if contents[k][i] == '0' and k in range(py, py + width):
-				contents[k][i] = '1'
-	
-	#flood_fill(contents, [px, py], [x, y], 100)
+	visited = []
+	visited.append([True for _ in range (0, x)])
+	for _ in range (0, y - 2):
+		visited.append([True] + [False for _ in range(0, x - 2)] + [True])
+	visited.append([True for _ in range (0, x)])
 
+	# print(visited)
+	start_x, start_y = random.randint(1, x - 1), random.randint(1, y - 1)
+	randomized_dfs(contents, start_x, start_y, visited)
 
+	
 def generate_map(filename, x, y) -> None:
-	contents = []
-
-	contents.append(['1' for _ in range(0, x)] + ['\n'])
-	for _ in range(0, y - 2):
-		contents.append(['1'] + list((x - 2) * "0") + ['1', '\n'])
-	contents.append(['1' for _ in range(0, x)] + ['\n'])
+	contents = [[WALL for _ in range(0, x)] for _ in range(0, y)]
 
 	generate_player_and_exit(contents, x, y)
 	generate_coins(contents, x, y)
@@ -95,8 +94,8 @@ def generate_map(filename, x, y) -> None:
 
 	f = open(filename, "x")
 	for line in contents:
-		print("".join(line), end = '') 
-		f.write("".join(line))
+		print("".join(line), end = '\n') 
+		f.write("".join(line) + '\n')
 	f.close()
 	print(f'The map was saved in {LRED}{filename}{RESET}.')
 
